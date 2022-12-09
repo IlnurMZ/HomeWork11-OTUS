4d87193057bb1d325ac001150ad0a3f0ae827d3d
MukIlnur <24mi@bk.ru>
2022-12-09 15:50:45 +0500

    Подправил значаения в словаре

--- HomeWork11-OTUS/Program.cs
+++ HomeWork11-OTUS/Program.cs
@@ -6,10 +6,11 @@
         {
             OtusDictionary otusDictionary = new OtusDictionary();
             
-            otusDictionary.Add(2, "Два");
+            otusDictionary.Add(100, "Сто");
             otusDictionary.Add(4, "Четыре");            
-            otusDictionary.Add(34, "Пять");            
-
+            otusDictionary.Add(34, "Тридцать четыре");
+            otusDictionary.Add(2, "Два");
+            
             Console.WriteLine(otusDictionary[2]); 
         }
     }
